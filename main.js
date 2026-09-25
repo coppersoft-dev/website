c5b59f4b9bde8434a434bb6ca566d7a8dbe3981e
Frederik Fournes <frederik@coppersoft.com>
2026-09-25 14:19:48 +0200

    Create new Migration Landing Page

--- main.js
+++ main.js
@@ -11,6 +11,7 @@
     <a href="/#services" data-de="Leistungen" data-en="Services"></a>
     <a href="/#approach" data-de="Ansatz" data-en="Approach"></a>
     <a href="/#domain" data-de="Domäne" data-en="Domain"></a>
+    <a href="/migration.html" data-de="Migration 2029" data-en="Migration 2029"></a>
     <a href="/#contact" data-de="Kontakt" data-en="Contact"></a>
   </div>
   <div class="navactions">
